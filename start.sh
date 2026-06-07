d3b0ea8a04073882e8d6f769e5ca90efd686cd57
javiersantana <javier.santana.12@ull.edu.es>
2026-06-07 13:12:50 +0100

    changed start script

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
-#!/bin/bash                                                                                                                            
-
-#start.sh — Labyrinth Madness (Processing/Java):                                         
+#!/bin/bash
 
 rm -f /tmp/.X0-lock /tmp/.X11-unix/X0
-                                                                                                                                       
-xpra start :0 \                                                                         
-  --bind-tcp=0.0.0.0:8080 \                                                                                                          
-  --html=on \              
+
+xpra start :0 \
+  --bind-tcp=0.0.0.0:8080 \
+  --html=on \
   --start="java -cp /app/LabyrinthApp.jar:/app/core.jar labyrinth_madness.src.Main" \
-  --exit-with-children=yes \                                                                                                           
-  --daemon=no \             
-  --encoding=x264 \                                                                                                                    
-  --dpi=96 \                                                                            
-  --desktop-scaling=off        
+  --exit-with-children=yes \
+  --daemon=no \
+  --encoding=x264 \
+  --dpi=96 \
+  --desktop-scaling=off
